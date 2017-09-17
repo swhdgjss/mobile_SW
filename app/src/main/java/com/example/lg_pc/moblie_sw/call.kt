ca9eef1bbb019176f37ba0014e5d069aa0561e00
김영남 <bunpilgaroo@naver.com>
2017-09-17 15:07:06 +0900

    0917

--- app/src/main/java/com/example/lg_pc/moblie_sw/call.kt
+++ app/src/main/java/com/example/lg_pc/moblie_sw/call.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class call : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call)
--- app/src/main/java/com/example/lg_pc/moblie_sw/calling.kt
+++ app/src/main/java/com/example/lg_pc/moblie_sw/calling.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class calling : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calling)
